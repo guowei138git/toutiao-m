cdcad1b56b85dbb1638bccf295c60d1189c9a79c
disanyanshijie.gw <908592508@qq.com>
2023-04-11 17:59:03 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
     <!-- 路由的出口 -->
-    <router-view/>
+    <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <hr />
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="success">成功按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 <style lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import store from './store'
 
 // 加载全局样式
 import './styles/index.less'
-
+//  加载 Vant 核心组件库
+import Vant from 'vant'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
+
+// 注册使用 Vant组件库
+Vue.use(Vant)
 
 new Vue({
   router,
