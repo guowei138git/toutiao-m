564f36de862cefb7659cee62f1898c2574c3aba3
disanyanshijie.gw <908592508@qq.com>
2023-04-17 15:23:22 +0800

    04_样式调整（频道编辑组件）

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="channel-edit">
       <van-cell :border="false">
-          <div slot="title">我的频道</div>
+          <div slot="title" class="title-text">我的频道</div>
           <van-button
+          class="edit-btn"
           type="danger"
           plain
           round
@@ -10,6 +11,7 @@
       </van-cell>
       <van-grid :gutter="10">
           <van-grid-item 
+          class="grid-item"
           v-for="value in 8"
           :key="value"
           text="文字"
@@ -18,10 +20,11 @@
 
       <!-- 频道推荐 -->
       <van-cell>
-          <div slot="title">频道推荐</div>
+          <div slot="title" class="title-text">频道推荐</div>
       </van-cell>
       <van-grid :gutter="20">
           <van-grid-item
+          class="grid-item"
           v-for="value in 8"
           :key="value"
           text="推荐内容"
@@ -41,5 +44,26 @@ export default {
     .channel-edit {
         // 内边距设置 上下42px 左右0px
         padding: 42px 0;
+    }
+
+    .title-text {
+        font-size: 16px;
+        color: #333;
+    }
+
+    .edit-btn {
+        width: 52px;
+        height: 24px;
+        font-size: 13px;
+        color: #f85959;
+        border: 1px solid #f85959;
+    }
+
+   /deep/ .grid-item {
+        width: 80px;
+        height: 43px;
+        .van-grid-item__content {
+            background-color: #f4f5f6;
+        }
     }
 </style>
