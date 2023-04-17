e0e7cef124883aea23e389fcf880263595ecece0
disanyanshijie.gw <908592508@qq.com>
2023-04-17 09:34:03 +0800

    03_频道编辑组件-我的频道

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>频道编辑</div>
+  <div class="channel-edit">
+      <van-cell :border="false">
+          <div slot="title">我的频道</div>
+          <van-button
+          type="danger"
+          plain
+          round
+          size="mini">编辑</van-button>
+      </van-cell>
+      <van-grid :gutter="10">
+          <van-grid-item 
+          v-for="value in 8"
+          :key="value"
+          text="文字"
+          />
+      </van-grid>
+  </div>
 </template>
 
 <script>
@@ -8,6 +24,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+    .channel-edit {
+        // 内边距设置 上下42px 左右0px
+        padding: 42px 0;
+    }
 </style>
