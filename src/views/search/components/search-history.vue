69d4fa878ff9d6652c40e8a043f7d4bd0c7e9082
disanyanshijie.gw <908592508@qq.com>
2023-04-19 06:48:26 +0800

    3.4_搜索历史组件实现

--- src/views/search/components/search-history.vue
+++ src/views/search/components/search-history.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>搜索历史记录</div>
+  <div class="search-history">
+    <van-cell title="搜索历史">
+      <span>全部删除</span>
+      <span>完成</span>
+      <van-icon name="delete" />
+    </van-cell>
+    <van-cell title="hello" >
+      <van-icon name="close" />
+    </van-cell>
+     <van-cell title="hello1" >
+      <van-icon name="close" />
+    </van-cell>
+     <van-cell title="hello2" >
+      <van-icon name="close" />
+    </van-cell>
+  </div>
 </template>
 
 <script>
