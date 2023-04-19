716ebfbf133156cbecf86982e2fe033e6ee2b752
disanyanshijie.gw <908592508@qq.com>
2023-04-19 18:00:16 +0800

    请求获取数据-搜索结果

--- src/views/search/components/search-result.vue
+++ src/views/search/components/search-result.vue
@@ -7,14 +7,16 @@
     @load="onLoad"
     >
       <van-cell
-      v-for="item in list"
-      :key="item"
-      :title="item"></van-cell>
+      v-for="(article, index) in list"
+      :key="index"
+      :title="article.title"></van-cell>
     </van-list>
   </div>
 </template>
 
 <script>
+import { getSearchResult } from '@/api/search'
+
 export default {
     name:'SearchResult',
     props: {
@@ -27,11 +29,47 @@ export default {
       return {
         list: [],
         loading: false,
-        finished: false
+        finished: false,
+        page: 1,
+        perPage: 10
       }
     },
     methods: {
-      onLoad () {
+      async onLoad () {
+        try { 
+          // 1.请求获取数据 - 搜索结果
+          const { data } = await getSearchResult({
+            page: this.page, // 页码
+            per_page: this.perPage, // 每页大小
+            q: this.searchText  // 查询关键词
+          })
+          console.log(data)
+
+          // 2.将数据添加到数组中
+          const { results } = data.data
+          this.list.push(...results)
+
+          // 3.将本次加载中的 loading 关闭
+          this.loading = false
+
+         // 4.判断是否还有数据 ？
+         if (results.length) {
+            //  如果有，则更新获取下一页数据的 页面
+            this.page++
+         } else {
+           // 如果没有，则将加载状态 finished 设置为结束
+           this.finished = true
+         }
+
+        } catch (error) {
+          this.$toast('获取搜索结果失败，请稍后重试')
+        }
+        
+        
+     
+      },
+
+      onLoadV1 () {
         setTimeout(() => {
           for (let i = 0; i < 10; i++) {
             this.list.push(this.list.length + 1)
@@ -44,7 +82,7 @@ export default {
             this.finished = true
           }
         }, 1000)
-      }
+      },
     }
 }
 </script>
