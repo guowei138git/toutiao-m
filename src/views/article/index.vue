0768cd543108f843c92d316f38e3e81fb26d5480
disanyanshijie.gw <908592508@qq.com>
2023-04-20 15:47:30 +0800

    2.2_Loading加载

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -6,6 +6,18 @@
     title="黑马头条" 
     left-arrow > </van-nav-bar>
     <!-- /导航栏 -->
+
+    <div class="main-wrap">
+      <!-- 加载中 -->
+      <div class="loading-wrap">
+        <van-loading 
+        color="#3296fa"
+        vertical>加载中...</van-loading>
+      </div>
+      <!-- /加载中 -->
+
+    </div>
+
   </div>
 </template>
 
