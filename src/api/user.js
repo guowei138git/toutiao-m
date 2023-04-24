5b4685965cb529d915341e6d9f49214d14787fa6
disanyanshijie.gw <908592508@qq.com>
2023-04-25 07:54:49 +0800

    8.2_关注用户-功能实现

--- src/api/user.js
+++ src/api/user.js
@@ -6,45 +6,60 @@ import request from '@/utils/request'
 
 // 用户登录
 export const login = data => {
-    return request({
-        method: 'POST',
-        url: '/v1_0/authorizations',
-        data
-    })
+  return request({
+    method: 'POST',
+    url: '/v1_0/authorizations',
+    data
+  })
 }
 
 // 发送验证码
 export const sendSms = mobile => {
-    return request({
-      method: 'GET',
-      url: `/v1_0/sms/codes/${mobile}`
-    })
-  }
+  return request({
+    method: 'GET',
+    url: `/v1_0/sms/codes/${mobile}`
+  })
+}
 
-  // 获取用户自己的信息
-  export const getUserInfo = () => {
-    return request({
-      method: 'GET',
-      url: '/v1_0/user'
-    })
-  }
+// 获取用户自己的信息
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/v1_0/user'
+  })
+}
 
-    // // 获取用户自己的信息V1 - 已过时，不推荐再使用
-    // export const getUserInfoV1 = () => {
-    //   return request({
-    //     method: 'GET',
-    //     url: '/v1_0/user',
-    //     // 发送请求头数据
-    //     headers: {
-    //       Authorization: `Bearer ${store.state.user.token}`
-    //     }
-    //   })
-    // }
+// // 获取用户自己的信息V1 - 已过时，不推荐再使用
+// export const getUserInfoV1 = () => {
+//   return request({
+//     method: 'GET',
+//     url: '/v1_0/user',
+//     // 发送请求头数据
+//     headers: {
+//       Authorization: `Bearer ${store.state.user.token}`
+//     }
+//   })
+// }
 
-    // 获取用户自己的频道信息
-    export const getUserChannels = () => {
-      return request({
-        method: 'GET',
-        url: '/v1_0/user/channels'
-      })
+// 获取用户自己的频道信息
+export const getUserChannels = () => {
+  return request({
+    method: 'GET',
+    url: '/v1_0/user/channels'
+  })
+}
+
+/**
+     * 关注用户
+     * @param targetId - 关注用户的id
+     * @returns data
+     */
+export const addFollow = (targetId) => {
+  return request({
+    method: 'POST',
+    url: '/v1_0/user/followings',
+    data: {
+      target: targetId
     }
+  })
+}
