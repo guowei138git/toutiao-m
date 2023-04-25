ed8e136fd3f133332092aea454747fe8672a7e25
disanyanshijie.gw <908592508@qq.com>
2023-04-26 07:22:43 +0800

    6.1_准备组件-文章收藏

---
+++ src/components/collect-article/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <van-icon name="star-o" color="#777" />
+</template>
+
+<script>
+export default {
+  name: 'CollectArticle'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -28,14 +28,15 @@
           <div slot="label" class="publish-data">{{article.pubdate | relativeTime}}</div>
 
           <!-- 关注用户 -->
-          <!-- 
+          <!--
             模板中的 $event 是事件参数
-           -->
-          <follow-user 
+          -->
+          <follow-user
             class="follow-btn"
             :is-followed="article.is_followed"
             :user-id="article.aut_id"
-            @update-is_followed="article.is_followed = $event" />
+            @update-is_followed="article.is_followed = $event"
+          />
           <!-- <van-button
             v-if="article.is_followed"
             class="follow-btn"
@@ -54,7 +55,7 @@
             icon="plus"
             :loading="followLoading"
             @click="onFollow"
-          >关注</van-button> -->
+          >关注</van-button>-->
           <!-- /关注用户 -->
         </van-cell>
         <!-- /用户信息 -->
@@ -89,7 +90,10 @@
     <div class="article-bottom">
       <van-button class="comment-btn" type="default" round size="small">写评论</van-button>
       <van-icon name="comment-o" info="123" color="#777" />
-      <van-icon name="star-o" color="#777" />
+      <!-- 收藏 -->
+      <!-- <van-icon name="star-o" color="#777" /> -->
+      <collect-article class="btn-item" />
+      <!-- /收藏 -->
       <van-icon name="good-job-o" color="#777" />
       <van-icon name="share" color="#777" />
     </div>
@@ -99,10 +103,10 @@
 </template>
 
 <script>
-import { getArticleById } from "@/api/article";
-import { ImagePreview } from "vant";
-
+import { getArticleById } from '@/api/article'
+import { ImagePreview } from 'vant'
 import FollowUser from '@/components/follow-user'
+import CollectArticle from '@/components/collect-article'
 
 // ImagePreview([
 //   'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
@@ -111,14 +115,14 @@ import FollowUser from '@/components/follow-user'
 // ])
 
 export default {
-  name: "ArticleIndex",
-  data() {
+  name: 'ArticleIndex',
+  data () {
     return {
       article: {}, // 文章详情
       loading: true, // 加载中的 loading 状态
       errStatus: 0, // 失败的状态码
       followLoading: false // 关注的 loading
-    };
+    }
   },
   props: {
     // articleId: {
@@ -128,20 +132,21 @@ export default {
   },
   components: {
     // FollowUser:FollowUser,
-    FollowUser
+    FollowUser,
+    CollectArticle
   },
-  created() {
-    this.loadArticle();
+  created () {
+    this.loadArticle()
   },
   methods: {
-    async loadArticle() {
+    async loadArticle () {
       // 开启加载中的状态
-      this.loading = true;
+      this.loading = true
       try {
-        const articleId = this.$route.params.articleId;
-        console.log("articleId:", articleId);
-        const { data } = await getArticleById(articleId);
-        console.log("getArticleById resposne:", data);
+        const articleId = this.$route.params.articleId
+        console.log('articleId:', articleId)
+        const { data } = await getArticleById(articleId)
+        console.log('getArticleById resposne:', data)
         // 摸拟加载失败情况 仅用于测试
         // if (Math.random() > 0.5) {
         //   JSON.parse('danfsagklhaulkl')
@@ -149,41 +154,41 @@ export default {
         // 摸拟加载失败情况 仅用于测试
 
         // 数据驱动视图 这件事儿 不是立即的
-        this.article = data.data;
+        this.article = data.data
 
         // 初始化图片点击预览
         // console.log(this.$refs['article-content'])
         setTimeout(() => {
           // console.log(this.$refs['article-content'])
-          this.previewImage();
-        }, 0);
+          this.previewImage()
+        }, 0)
 
         // 请求成功， 关闭 Loading
         // this.loading = false
       } catch (error) {
         if (error.response && error.response.status === 404) {
-          this.errStatus = 404;
+          this.errStatus = 404
         }
         // 请求失败， 关闭 loading
         //  this.loading = false
-        console.log("获取数据失败", error);
+        console.log('获取数据失败', error)
       }
       // 无论成功还是失败，都需要关闭 loading
-      this.loading = false;
+      this.loading = false
     },
-    previewImage() {
+    previewImage () {
       // 得到文章内容的所有 dom
-      const articleContent = this.$refs["article-content"];
+      const articleContent = this.$refs['article-content']
       // 得到所有的 img 节点
-      const imgs = articleContent.querySelectorAll("img");
-      console.log(imgs);
+      const imgs = articleContent.querySelectorAll('img')
+      console.log(imgs)
       // 定义一个数组
-      const imagesArr = [];
+      const imagesArr = []
       // 遍历所有 img 节点
       imgs.forEach((img, index) => {
-        const imageURl = this.getCaption(img.src, "url=");
-        console.log("imageURl=", imageURl);
-        imagesArr.push(imageURl);
+        const imageURl = this.getCaption(img.src, 'url=')
+        console.log('imageURl=', imageURl)
+        imagesArr.push(imageURl)
         // 给每个img节点注册点击事件
         img.onclick = () => {
           // 在 img 点击事件处理函数中  调用 ImagePreview 预览
@@ -192,21 +197,20 @@ export default {
             images: imagesArr,
             // 起始位置  从 0 开始
             startPosition: index
-          });
-        };
-      });
-      console.log(imagesArr);
+          })
+        }
+      })
+      console.log(imagesArr)
     },
     // 截取指定字符后面的内容
-    getCaption(str, subStr) {
-      const index = str.lastIndexOf(subStr);
-      const res = str.substring(index + subStr.length, str.length);
-      console.log(res);
-      return res;
+    getCaption (str, subStr) {
+      const index = str.lastIndexOf(subStr)
+      const res = str.substring(index + subStr.length, str.length)
+      console.log(res)
+      return res
     }
-   
   }
-};
+}
 </script>
 
 <style scoped lang="less">
@@ -309,28 +313,33 @@ export default {
     right: 0;
     bottom: 0;
     display: flex;
-    height: 44px;
+    // height: 44px;
     justify-content: space-around;
     align-items: center;
     box-sizing: border-box;
+    height: 88px;
     border-top: 1px solid #d8d8d8;
     background-color: #fff;
 
     .comment-btn {
-      width: 141px;
-      height: 23px;
+      width: 282px;
+      height: 46px;
       border: 2px solid #eee;
-      font-size: 15px;
-      line-height: 23px;
+      font-size: 30px;
+      line-height: 46px;
       color: #a7a7a7;
     }
 
-    .van-icon {
-      font-size: 20px;
-      .van-ifno {
-        font-size: 8px;
-        background-color: #e22829;
-      }
+    /deep/ .van-icon {
+      font-size: 40px;
+    }
+
+    .btn-item {
+      border: none;
+      padding: 0;
+      height: 40px;
+      line-height: 40px;
+      color: #777;
     }
   }
 }
