19d5fa3fe47a32a81eb04c0b61924c93f0eea947
disanyanshijie.gw <908592508@qq.com>
2023-04-26 10:14:25 +0800

    6.5_封装收藏和取消文章的接口api

--- src/api/article.js
+++ src/api/article.js
@@ -21,3 +21,22 @@ export const getArticleById = (articleId) => {
         url: `/v1_0/articles/${articleId}`
     })
 }
+
+// 收藏文章
+export const addCollect = (target) => {
+    return request({
+        method: 'POST',
+        url: '/v1_0/article/collections',
+        data: {
+            target
+        }
+    })
+}
+
+// 取消收藏文章
+export const deleteCollect = (target) =>{
+    return request({
+        method: 'DELETE',
+        url: `/v1_0/article/collections/${target}`
+    })
+}
--- src/components/collect-article/index.vue
+++ src/components/collect-article/index.vue
@@ -1,12 +1,17 @@
 <template>
-  <van-button 
-  :icon="value ? 'star' : 'star-o'" 
-  :class="{
+  <van-button
+    :icon="value ? 'star' : 'star-o'"
+    :class="{
         collected: value
-    }" />
+    }"
+    :loading="loading"
+    @click="onCollect"
+  />
 </template>
 
 <script>
+import { addCollect, deleteCollect } from "@/api/article";
+
 export default {
   name: "CollectArticle",
   // 使用v-model 必须使用 value名来接收
@@ -14,6 +19,28 @@ export default {
     value: {
       type: Boolean,
       required: true
+    }
+  },
+  data() {
+    return {
+      loding: false
+    };
+  },
+  methods: {
+    async onCollect() {
+      this.loding = true;
+      try {
+          if (this.value) {
+              // 已收藏 -> 取消收藏
+
+          } else {
+              // 未收藏 -> 添加收藏
+
+          }
+      } catch (error) {
+          this.$toast.fail('操作失败，请重试')
+      }
+      this.loding = false;
     }
   }
 };
